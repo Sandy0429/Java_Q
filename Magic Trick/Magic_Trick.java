16556dd4e8313e5db30e25143a745dbaa6f84198
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-10-22 22:20:53 +0530

    Create Magic_Trick.java

---
+++ Magic Trick/Magic_Trick.java
@@ -0,0 +1,22 @@
+static void magicTrick(int a, int b){
+	    /* Write your code here*/		
+		System.out.println(a+b/2);
+	}
+
+/*
+Answer:
+A+B/2
+
+Explanation:
+1) x
+
+2) x+A
+
+3) 2(x+A)
+
+4) 2(x+A)+ B → 2(x+A+B/2)
+
+5) 2(X+A+B/2)/2 → X+A+B/2
+
+6) X+A+B/2-X == A+B/2
+*/
