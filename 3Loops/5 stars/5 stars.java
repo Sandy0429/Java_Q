2c4fa8bd8f8938b7f68495ce0edafbbb6ff60e11
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-10-27 02:30:57 +0530

    Create 5 stars.java

---
+++ 3Loops/5 stars/5 stars.java
@@ -0,0 +1,11 @@
+static void vertical5(){
+//Enter your code here
+    for(int i=0;i<5;i++)
+        System.out.println("*");
+}
+
+static void horizontal5(){
+//Enter your code here
+    for(int i=0;i<5;i++)
+            System.out.print("* ");
+}
