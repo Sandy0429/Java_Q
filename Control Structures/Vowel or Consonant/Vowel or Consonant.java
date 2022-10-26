b98af8cc44ec118528bb0a9f1989c22d1d178c4f
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-10-27 02:25:42 +0530

    Create Vowel or Consonant.java

---
+++ Control Structures/Vowel or Consonant/Vowel or Consonant.java
@@ -0,0 +1,15 @@
+static void VowelorConsonant(char C){
+//Enter your code here
+    switch(C)
+    {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+            System.out.print("V");
+        break;
+        default:
+            System.out.print("C");
+    }
+}
