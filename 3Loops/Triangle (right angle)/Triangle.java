ff712d29da19c3be380a11e58537888a12a1d2b6
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-10-27 02:39:05 +0530

    Create Triangle.java

---
+++ 3Loops/Triangle (right angle)/Triangle.java
@@ -0,0 +1,9 @@
+static void printTriangle(){
+// Your code here
+    for(int i=0;i<5;i++)
+    {
+        for(int j =0;j<=i;j++)
+            System.out.print("* ");
+        System.out.println();
+    }
+}
