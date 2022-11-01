3c7c92c999ae98326aabf3bf905fcfe1297d62a7
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-11-01 21:46:10 +0530

    Create Data types.java

---
+++ Data types/Data types.java
@@ -0,0 +1,12 @@
+static void printDataTypes(int a, long b, float c, double d, char e)
+{
+        /* Write your code here*/
+
+        System.out.println(a);
+        System.out.println(b);
+        //System.out.println(Math.round(c*100.0)/100.0);
+        //System.out.println(Math.round(d*10000.0)/10000.0);
+        System.out.printf("%.2f \n",c);
+        System.out.printf("%.4f \n",d);
+        System.out.println(e);
+}
