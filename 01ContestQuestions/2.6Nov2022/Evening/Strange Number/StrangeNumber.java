28a18bedb4dd28137006655b572a48a191c641df
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-11-17 14:08:06 +0530

    Create StrangeNumber.java

---
+++ 01ContestQuestions/2.6Nov2022/Evening/Strange Number/StrangeNumber.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+                      // Your code here
+        Scanner sc =  new Scanner(System.in);
+        int x = sc.nextInt();
+
+        System.out.println(((x-1)*9));
+    }
+}
