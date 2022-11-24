ea8fda0b6965cba91de9c5e9e723e15ca8bbf0f4
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-11-25 00:25:00 +0530

    Create CountFrequencyOfCharactersInList.java

---
+++ 0Assignment/03-ArrayList/Count Frequency of Characters in List/CountFrequencyOfCharactersInList.java
@@ -0,0 +1,16 @@
+// Function to insert element
+    public static void insert(ArrayList<Character> clist, char c)
+    {
+        // Your code here
+        clist.add(c);
+    }
+    
+    // Function to count frequency of element
+    public static void freq(ArrayList<Character> clist, char c)
+    {
+        // Your code here
+        if(clist.contains(c))
+            System.out.println(Collections.frequency(clist, c));
+        else
+            System.out.println("Not Present");
+    }
