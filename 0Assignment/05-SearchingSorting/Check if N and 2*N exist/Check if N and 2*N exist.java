208d11f9b7085deead1d74f2dd4b74b7238c594e
Sandy0429 <54345790+Sandy0429@users.noreply.github.com>
2022-12-14 22:02:27 +0530

    Create Check if N and 2*N exist.java

---
+++ 0Assignment/05-SearchingSorting/Check if N and 2*N exist/Check if N and 2*N exist.java
@@ -0,0 +1,60 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+      public static int binarySearch(int[] arr, int target) {
+        int left  =0 ;
+        int right  = arr.length -1;
+
+        while(left<=right) {
+            int mid = left + (right - left) /2;
+
+            if(arr[mid] == target) {
+                return mid;
+            } else if (target > arr[mid]) {
+                left = mid  +1;
+            } else {
+                right = mid -1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    
+    public static boolean checking2n(int[] arr) {
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+            int binarySearchAnswer = binarySearch(arr, 2*element);
+            if(binarySearchAnswer != -1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+  
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+
+        boolean ans = checking2n(arr);
+        if(ans) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+
+    }
+
+}
